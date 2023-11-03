5b8f4a95b980b6b94538e074118c4aa11ca77398
Shadman <shadman.alizada@gmail.com>
2023-11-03 23:31:54 +0400

    feat: created Detail page

---
+++ src/pages/Detail/Detail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function Detail() {
+  return (
+    <SafeAreaView>
+      <Text>Detail</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Detail;
