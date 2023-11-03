889456741025e1437d68b631445ad56dffd0b718
Shadman <shadman.alizada@gmail.com>
2023-11-04 01:02:23 +0400

    feat: created Error animation with Lottie

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {FlatList, SafeAreaView, ActivityIndicator, Text} from 'react-native';
+import {FlatList, SafeAreaView} from 'react-native';
 import Config from 'react-native-config';
 import {ProductTypes} from './types';
 
 import ProductCard from '../../components/ProductCard';
 import styles from './Products.style';
 import useFetch from '../../hooks/useFetch';
+import Loading from '../../components/common/Loading';
+import Error from '../../components/common/Error';
 let URL: string | any = Config.API_URL;
 
 function Products() {
@@ -15,11 +17,8 @@ function Products() {
     return <ProductCard product={item} />;
   };
 
-  if (isLoading) {
-    return <ActivityIndicator size="large" />;
-  }
   if (error) {
-    return <Text>{error}</Text>;
+    return <Error error={error} />;
   }
 
   return (
