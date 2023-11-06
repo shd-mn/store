98c6b857c19c53f51721d79cba231df8e2c55e4f
Shadman <shadman.alizada@gmail.com>
2023-11-06 22:25:33 +0400

    feat: built Detail Page, fetch data used custom hook

--- src/pages/Detail/Detail.style.ts
+++ src/pages/Detail/Detail.style.ts
@@ -1,3 +1,42 @@
-import {StyleSheet} from 'react-native';
-
-export default StyleSheet.create({});
+import {StyleSheet, Dimensions} from 'react-native';
+const window = Dimensions.get('window');
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+  },
+  img_box: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  img: {
+    flex: 1,
+    width: window.width,
+  },
+  content_container: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  description: {
+    marginBottom: 10,
+  },
+  info: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  price: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  rating: {fontSize: 18},
+});
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Image, SafeAreaView, Text, View, Button} from 'react-native';
+import useFetch from '../../hooks/useFetch';
+import Config from 'react-native-config';
+import Loading from '../../components/common/Loading';
+import Error from '../../components/common/Error';
 
-function Detail() {
+import styles from './Detail.style';
+type PropTypes = {
+  route: {
+    params: {
+      id: string | number;
+    };
+  };
+};
+
+let URL: string | any = Config.API_URL;
+
+function Detail({route}: PropTypes) {
+  const {id} = route.params;
+  const {data: product, isLoading, error} = useFetch(`${URL}/${id}`);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error error={error} />;
+  }
+
+  const {title, image, description, price, rating} = product[0];
+
   return (
-    <SafeAreaView>
-      <Text>Detail</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.img_box}>
+        <Image style={styles.img} source={{uri: image}} resizeMode="contain" />
+      </View>
+      <View style={styles.content_container}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <View style={styles.info}>
+          <Text style={styles.rating}>{rating.rate} rate</Text>
+          <Text style={styles.price}>{price} $</Text>
+        </View>
+        <Button title="buy" />
+      </View>
     </SafeAreaView>
   );
 }
