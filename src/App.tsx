b95ae82ecea45b5ea3171313c478bc11988a4e2b
Shadman <shadman.alizada@gmail.com>
2023-11-11 23:58:54 +0400

    feat: added  Redux-toolkit to project

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,39 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Products from './pages/Products';
 import Detail from './pages/Detail';
+import MainProvider from './redux/Provider';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor: '#60a5fa',
-          },
-          headerTintColor: 'white',
-        }}>
-        <Stack.Screen
-          name="ProductsPage"
-          component={Products}
-          options={{
-            title: 'Shop',
-          }}
-        />
-        <Stack.Screen
-          name="DetailPage"
-          component={Detail}
-          options={{
-            title: 'Detail',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#60a5fa',
+            },
+            headerTintColor: 'white',
+          }}>
+          <Stack.Screen
+            name="ProductsPage"
+            component={Products}
+            options={{
+              title: 'Shop',
+            }}
+          />
+          <Stack.Screen
+            name="DetailPage"
+            component={Detail}
+            options={{
+              title: 'Detail',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MainProvider>
   );
 }
 
---
+++ src/redux/Provider.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from './store';
+function MainProvider({children}: {children: any}) {
+  return <Provider store={store}>{children}</Provider>;
+}
+
+export default MainProvider;
