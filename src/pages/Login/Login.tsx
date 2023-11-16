3a2b6c0339267a3149517f44ce7ede3a2d89c269
Shadman <shadman.alizada@gmail.com>
2023-11-16 23:59:52 +0400

    feat: used Formik in Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import {SafeAreaView, View, Button, Image} from 'react-native';
-
+import {Formik} from 'formik';
 import styles from './Login.style';
 import Input from '../../components/Input';
 
 function Login() {
-  const onChange = () => {};
-  const handleSubmit = () => {};
+  const handleSubmitForm = (value: any) => {
+    console.log(value);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.imageBox}>
         <Image
           style={styles.image}
           source={require('../../assets/images/shopping-bag.png')}
-          alt="logo"
         />
       </View>
-      <View style={styles.inputContainer}>
-        <Input placeholder="Username" onType={onChange} />
-        <Input placeholder="Pasword" onType={onChange} />
-        <Button title="Sign In" onPress={handleSubmit} />
-      </View>
+      <Formik
+        initialValues={{username: '', password: ''}}
+        onSubmit={handleSubmitForm}>
+        {({handleChange, handleSubmit, values}) => (
+          <View style={styles.inputContainer}>
+            <Input
+              placeholder="Username"
+              onType={handleChange('username')}
+              value={values.username}
+            />
+            <Input
+              placeholder="Password"
+              onType={handleChange('password')}
+              value={values.password}
+              secureText={true}
+            />
+            <Button title="Sign In" onPress={handleSubmit} />
+          </View>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 }
