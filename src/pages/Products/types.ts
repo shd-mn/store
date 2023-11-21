798509b8610d0813e59514e4ca7fab6dc7bcb9c8
Shadman <shadman.alizada@gmail.com>
2023-11-21 23:53:54 +0400

    feat: added UserTypes

--- src/pages/Products/types.ts
+++ src/pages/Products/types.ts
@@ -12,3 +12,32 @@ export interface Rating {
   rate: number;
   count: number;
 }
+
+export interface UserTypes {
+  address: Address;
+  id: number;
+  email: string;
+  username: string;
+  password: string;
+  name: Name;
+  phone: string;
+  __v: number;
+}
+
+export interface Address {
+  geolocation: Geolocation;
+  city: string;
+  street: string;
+  number: number;
+  zipcode: string;
+}
+
+export interface Geolocation {
+  lat: string;
+  long: string;
+}
+
+export interface Name {
+  firstname: string;
+  lastname: string;
+}
