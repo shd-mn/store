a10435c43e110ff48e881ef8529148d85ebf7a09
Shadman <shadman.alizada@gmail.com>
2023-11-24 23:52:53 +0400

    feat: created authentication flows

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,34 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Products from './pages/Products';
 import Detail from './pages/Detail';
 import MainProvider from './redux/Provider';
 import Login from './pages/Login';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
+  const [isSignedIn, setIsSignedIn] = useState(true);
+
+  useEffect(() => {
+    const getUserValue = async () => {
+      try {
+        const user = await AsyncStorage.getItem('user');
+        setIsSignedIn(user !== undefined);
+      } catch (e) {
+        // save error
+        console.log(`save error ${e}`);
+      }
+    };
+
+    getUserValue();
+  }, []);
+
   return (
     <MainProvider>
       <NavigationContainer>
@@ -24,25 +41,30 @@ function App(): JSX.Element {
             },
             headerTintColor: 'white',
           }}>
-          <Stack.Screen
-            name="LoginPage"
-            component={Login}
-            options={{title: 'Login'}}
-          />
-          <Stack.Screen
-            name="ProductsPage"
-            component={Products}
-            options={{
-              title: 'Shop',
-            }}
-          />
-          <Stack.Screen
-            name="DetailPage"
-            component={Detail}
-            options={{
-              title: 'Detail',
-            }}
-          />
+          {isSignedIn ? (
+            <>
+              <Stack.Screen
+                name="ProductsPage"
+                component={Products}
+                options={{
+                  title: 'Shop',
+                }}
+              />
+              <Stack.Screen
+                name="DetailPage"
+                component={Detail}
+                options={{
+                  title: 'Detail',
+                }}
+              />
+            </>
+          ) : (
+            <Stack.Screen
+              name="LoginPage"
+              component={Login}
+              options={{title: 'Login'}}
+            />
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </MainProvider>
