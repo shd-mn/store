6c387014aa05b3d7597bfe772ca48a66d85381d2
Shadman <shadman.alizada@gmail.com>
2023-11-25 23:58:54 +0400

    refacor: useFetchUser

--- src/hooks/useFetchUser.tsx
+++ src/hooks/useFetchUser.tsx
@@ -1,28 +1,17 @@
 import {useState} from 'react';
 import axios from 'axios';
 import {UserTypes} from '../pages/Products/types';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-type AuthData = {
-  username: string;
-  password: string;
-};
 function useFetchUser() {
   const [data, setData] = useState<UserTypes[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
 
-  const fetchUsers = async (URL: string | any, values: AuthData) => {
+  const fetchUsers = async (URL: string | any) => {
     try {
       const respone = await axios<UserTypes[]>(URL);
       if (respone.data) {
-        const user = respone?.data.filter(
-          item =>
-            item.username === values.username &&
-            item.password === values.password,
-        );
-        setData(user);
-        await AsyncStorage.setItem('user', JSON.stringify(user));
+        setData(respone.data);
       }
     } catch (err: any) {
       setError(err.message);
